84df9b22cb2f71af391bfa1b86dce25baa9fe2cb
cynter <cynteran27@gmail.com>
2016-08-22 20:30:25 -0300

    random + for php

--- index.php
+++ index.php
@@ -15,4 +15,18 @@ echo '<h2>Hola PHP soy $nombre </h2>';
 echo "<h2>Hola PHP soy $nombre </h2>";
 echo '<h2>Hola PHP soy '.$nombre ."</h2>";
 echo "<br>";
+
+echo (rand() ."<br>");
+echo(rand(10,12));
+echo "<br>";
+echo "<br>";
+
+
+$veces=10;
+for ($i=0; $i < $veces; $i++) { 
+	# code...
+	echo(rand(10,100));
+	echo "<br>";
+}
+
 ?>
