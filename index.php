37e41a7210ceb3de856e3b875c172cb1703aceba
cynter <cynteran27@gmail.com>
2016-08-22 21:23:46 -0300

    porcentaje de cuanto sale cada numero

--- index.php
+++ index.php
@@ -1,32 +1,91 @@
-hola mundo
-<html>
-<head>
-	<title></title>
-</head>
-<body>
-	<!para comentar>
-<h1>hola mundo</h1> 
-</body>
-</html>
+
 
 <?php
-$nombre="Cynthia";
-echo '<h2>Hola PHP soy $nombre </h2>';
-echo "<h2>Hola PHP soy $nombre </h2>";
-echo '<h2>Hola PHP soy '.$nombre ."</h2>";
-echo "<br>";
-
-echo (rand() ."<br>");
-echo(rand(10,12));
-echo "<br>";
-echo "<br>";
-
-
 $veces=10;
-for ($i=0; $i < $veces; $i++) { 
-	# code...
-	echo(rand(10,100));
-	echo "<br>";
+
+$cont0=0;
+$cont1=0;
+$cont2=0;
+$cont3=0;
+$cont4=0;
+$cont5=0;
+$cont6=0;
+$cont7=0;
+$cont8=0;
+$cont9=0;
+
+for ($i=0; $i < $veces; $i++) {
+$random=rand(0,9);
+
+switch ($random) {
+	case 0;
+		
+		$cont0++;
+		break;
+
+	case 1;
+		# code...
+		$cont1++;
+		break;
+
+	case 2;
+		# code...
+		$cont2++;
+		break;
+
+	case 3;
+		# code...
+		$cont3++;
+		break;
+
+	case 4;
+		# code...
+		$cont4++;
+		break;
+
+	case 5;
+		# code...
+		$cont5++;
+		break;
+
+	case 6;
+		# code...
+		$cont6++;
+		break;
+
+	case 7;
+		# code...
+		$cont7++;
+		break;
+
+	case 8;
+		# code...
+		$cont8++;
+		break;
+
+	case 9;
+		# code...
+		$cont9++;
+		break;
+
+	
+	default:
+		# code...
+		break;
 }
+
+}
+
+echo "El numero aparecio ".($veces*$cont0)/100 ."%";
+echo "El numero aparecio ".($veces*$cont1)/100 ."%";
+echo "El numero aparecio ".($veces*$cont2)/100 ."%";
+echo "El numero aparecio ".($veces*$cont3)/100 ."%";
+echo "El numero aparecio ".($veces*$cont4)/100 ."%";
+echo "El numero aparecio ".($veces*$cont5)/100 ."%";
+echo "El numero aparecio ".($veces*$cont6)/100 ."%";
+echo "El numero aparecio ".($veces*$cont7)/100 ."%";
+echo "El numero aparecio ".($veces*$cont8)/100 ."%";
+echo "El numero aparecio ".($veces*$cont9)/100 ."%";
+
 
 ?>
